00460bc33acf63bc22b1dd0cfb3d51c38544eb11
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2024-01-31 21:23:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
 function daysOfYear(year) {
 	let days=0;
-	if(year%400===0)days=366;
-	else if(year%100==0)days=365;
-	else if(year%4===0)days=366;
+	if(year % 400===0)days=366;
+	else if(year % 100===0)days=365;
+	else if(year % 4===0)days=366;
 	else days=365;
 	return days;
 }
